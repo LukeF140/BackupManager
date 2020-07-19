b820b9fb2f5a2516a5e1c0df7263e2bfcb28abf7
LukeF140 <68498152+LukeF140@users.noreply.github.com>
2020-07-19 14:22:20 +0100

    Added The Project Files

---
+++ BackupManager.py
@@ -0,0 +1,69 @@
+"""
+---------- BACKUP MANAGER ----------
+Please read the README.txt for full details on the program and how it takes input and output
+"""
+
+# IMPORTS
+import zipfile
+import os
+from os.path import basename
+import re
+from datetime import datetime
+
+
+# VARIABLES
+backups = {}
+now = datetime.now()
+
+
+# FUNCTIONS
+# Reads file to find BackUps
+def BackupsToCompleate():
+    backupToDo = {}
+    try:
+        # Get Backup File
+        backupFile = open("BackupLocs.txt", "r")
+        fileCont = backupFile.readlines()
+        backupFile.close()
+        backUpFileInfo = []
+        # Remove \n
+        for E in fileCont:
+            E = re.sub('\\n', '', E)
+            if E != ' ':
+                backUpFileInfo.append(E)
+        # Sorts out directorys into dictionary
+        for i in range(0, len(backUpFileInfo)):
+            if i%2 == 0:
+                try:
+                    backupToDo[backUpFileInfo[i]] = backUpFileInfo[i+1]
+                except:
+                    print("ERROR: NO DICTIONARY FOR BACKUP TO BE PLACED")
+                    return {}
+        return backupToDo
+    except:
+        print("NO BACKUPS TO COMPLEATE")
+        return {}
+
+# Creates A BackUp (dirToBack: Directory to be backed up; backUpDir: Directory for the backup to be placed
+def CreateBackUp(dirToBack, backUpDir):
+    # Create Name of Backup Folder
+    backUpDir = backUpDir + "\\" + basename(dirToBack) + "_BACKUP--" + now.strftime("%d-%m-%Y_%H-%M") +".zip"
+    print("\nBACKING UP DIRECTORY:  '" + dirToBack + "',  TO DIRECTORY:  '" + backUpDir + "'")
+    # Create a ZipFile object
+    with zipfile.ZipFile(backUpDir, 'w') as zipObj:
+       # Go through all files in a Directory
+       for folderName, subfolders, filenames in os.walk(dirToBack):
+           for filename in filenames:
+               # Create compleate file path for file
+               filePath = os.path.join(folderName, filename)
+               print("BACKING UP:", filePath)
+               # Add file to zip file
+               zipObj.write(filePath, os.path.relpath(filePath, dirToBack), zipfile.ZIP_DEFLATED)
+    print("BACK UP COMPLEATED OF:  '" + dirToBack + "',  TO DIRECTORY:  '" + backUpDir + "'\n")
+
+
+# MAIN
+backups = BackupsToCompleate()
+for k in backups:
+    CreateBackUp(k, backups[k])
+
---
+++ README.txt
@@ -0,0 +1,18 @@
+BACKUP MANAGER
+Created by Luke Fairs
+
+This program will backup all the directorys to a defined location
+
+This program gets the directorys and locations from the file 'BackupLocs.txt'
+This file should be organised into pairs like stated.
+	The first line of the pair should be the directory to be backed up:
+		eg. C:\Users\ExampleUser\Pictures
+	The second line of the pair should be the location where the backup file should be placed:
+    		eg. C:\Backups
+
+The program will then create your backup when ever ran
+
+The backup will be a .zip folder using the following name covention:
+	nameoffolder_BACKUP--DD-MM-YYYY_HH-MM.zip
+
+This program is still under development and will change overtime as new features and systems are implemented.
