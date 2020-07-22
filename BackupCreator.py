1c4006f08d59fcd1e7c20ec6aabd2d4dd9ef3972
LukeF140 <68498152+LukeF140@users.noreply.github.com>
2020-07-22 16:06:53 +0100

    Specific Backup Function & Further "User-Proofing"
    Ability to only do a specific backup
    Further "User-Proofing" so that it doesn't through errors

--- BackupCreator.py
+++ BackupCreator.py
@@ -47,22 +47,25 @@ def BackupsToCompleate():
 # Creates A BackUp (dirToBack: Directory to be backed up; backUpDir: Directory for the backup to be placed)
 def CreateBackUp(dirToBack, backUpDir):
     if os.path.exists(dirToBack):
-        # Create Name of Backup Folder
-        backUpDir = backUpDir + "\\" + basename(dirToBack) + "_BACKUP--" + now.strftime("%d-%m-%Y_%H-%M") +".zip"
-        print("\nBACKING UP DIRECTORY:  '" + dirToBack + "',  TO DIRECTORY:  '" + backUpDir + "'")
-        # Create a ZipFile object
-        with zipfile.ZipFile(backUpDir, 'w') as zipObj:
-           # Go through all files in a Directory
-           for folderName, subfolders, filenames in os.walk(dirToBack):
-               for filename in filenames:
-                   # Create compleate file path for file
-                   filePath = os.path.join(folderName, filename)
-                   print("BACKING UP:", filePath)
-                   # Add file to zip file
-                   zipObj.write(filePath, os.path.relpath(filePath, dirToBack), zipfile.ZIP_DEFLATED)
-        print("BACK UP COMPLEATED OF:  '" + dirToBack + "',  TO DIRECTORY:  '" + backUpDir + "'\n")
+        if os.path.exists(backUpDir):
+            # Create Name of Backup Folder
+            backUpDir = backUpDir + "\\" + basename(dirToBack) + "_BACKUP--" + now.strftime("%d-%m-%Y_%H-%M") +".zip"
+            print("\nBACKING UP DIRECTORY:  '" + dirToBack + "',  TO DIRECTORY:  '" + backUpDir + "'")
+            # Create a ZipFile object
+            with zipfile.ZipFile(backUpDir, 'w') as zipObj:
+               # Go through all files in a Directory
+               for folderName, subfolders, filenames in os.walk(dirToBack):
+                   for filename in filenames:
+                       # Create compleate file path for file
+                       filePath = os.path.join(folderName, filename)
+                       print("BACKING UP:", filePath)
+                       # Add file to zip file
+                       zipObj.write(filePath, os.path.relpath(filePath, dirToBack), zipfile.ZIP_DEFLATED)
+            print("BACK UP COMPLEATED OF:  '" + dirToBack + "',  TO DIRECTORY:  '" + backUpDir + "'\n")
+        else:
+            print("ERROR: DIRECTORY '" + backUpDir + "' WAS NOT FOUND")
     else:
-        print("ERROR: Directory '" + dirToBack + "' was not found")
+        print("ERROR: DIRECTORY '" + dirToBack + "' WAS NOT FOUND")
 
 # Called from the main mannager
 def Main():
--- BackupManager.py
+++ BackupManager.py
@@ -96,6 +96,42 @@ def List():
     else:
         print("NO BACKUP PATHS")
 
+def SpecificBackup():
+    backupList = BackupList()
+    backupPaths = []
+    if backupList != {}:
+        for k in backupList:
+            backupPaths.append(k)
+
+        for i in range(0, len(backupPaths)):
+            print(str(i+1) + ". " + backupPaths[i])
+
+        try:
+            backDir = int(input("Which directory do you want to backup (enter the number): ")) - 1
+        except:
+            print ("ERROR: Number must be an Integer")
+            return
+            
+        if backDir >= 0 and backDir < len(backupPaths):
+            backupFile = open("BackupLocs.txt", "r")
+            lines = backupFile.readlines()
+            backupFile.close()
+
+            backupLocInfo = []
+            # Remove \n
+            E = re.sub('\\n', '', lines[backDir * 2])
+            if E != ' ':
+                backupLocInfo.append(E)
+            E = re.sub('\\n', '', lines[backDir * 2 + 1])
+            if E != ' ':
+                backupLocInfo.append(E)
+            
+            BackupCreator.CreateBackUp(backupLocInfo[0], backupLocInfo[1])
+        else:
+            print("ERROR: NUMBER ENTERED IS TOO LARGE OR SMALL")
+    else:
+        print ("THERE ARE NO DIRECTORYS SET UP TO BACKUP")
+
     
 # MAIN
 while contin:
@@ -105,6 +141,8 @@ while contin:
         Help()
     elif whatDo == "BACKUP" or whatDo == "BACK UP":
         BackupCreator.Main()
+    elif whatDo == "SPECIFIC" or whatDo == "SPECIFIC BACKUP" or whatDo == "SPECIFIC BACK UP":
+        SpecificBackup()
     elif whatDo == "RESTORE":
         BackupRestore.Main()
     elif whatDo == "LIST" or whatDo == "LIST BACKUP" or whatDo == "LIST BACK UP":
